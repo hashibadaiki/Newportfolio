4706cbf77c7effab5991a9a8f835aedeac37fa8a
hashibadaiki <higemeganeking@gmail.com>
2020-01-07 22:20:50 +0900

    ハンバーガーメニューまで完成 TODO ナビゲーション押したらそこに移動 パンクズリスト スタイルの修正（テキストのフォントサイズ、カラーの調整）

--- modal.js
+++ modal.js
@@ -1,1 +1,34 @@
-a
+$(function () {
+  $('.humbuggerBtn').click(function () {
+    
+    if($('.menuBtn').hasClass('active')){
+      $('.menuBtn').removeClass('active');
+    } else {
+      $('.menuBtn').addClass('active');
+    }
+    
+    if($('.navigation,.navigationList').hasClass('active')){
+      $('.navigation,.navigationList').removeClass('active');
+    } else {
+      $('.navigation,.navigationList').addClass('active');
+    }
+  });
+  
+  $('.fa-times').click(function () {
+    
+    if($('.navigation,.navigationList').hasClass('active')){
+      $('.navigation,.navigationList').removeClass('active');
+    } else {
+      $('.navigation,.navigationList').addClass('active');
+    }
+
+    if($('.menuBtn').hasClass('active')){
+      $('.menuBtn').removeClass('active');
+    } else {
+      $('.menuBtn').addClass('active');
+    }
+
+  });
+
+
+});
